ce93060cccb692a6a06fe4263d0729a8aa3d203a
Miningflo <debosscher.flore@gmail.com>
2021-06-24 00:58:14 +0200

    Updated main to include scheduling

--- main.py
+++ main.py
@@ -5,21 +5,33 @@
 # send image to discord channel
 
 import datetime
+import time
 from pprint import pp
+
+import pause as pause
+
 from SGP4.parser import readTle
 import requests
 from config import config
 from SGP4.passes_api import passes_all
 
 
-def unix2date(unix):
+def timestamp(unix):
     unix = int(unix)
-    return datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M')
+    return datetime.datetime.fromtimestamp(unix)
 
 
-def task(name, freq):
+def readable(timestamp):
+    return timestamp.strftime('%Y-%m-%d %H:%M')
+
+
+def task(name, freq, end):
     # It is now time
     # Run the task of imaging at frequency
+    print(f"Imaging {name} on {freq} MHz")
+    while int(time.time()) < end:
+        # Do the imaging
+        pass
     main()
 
 
@@ -34,9 +46,9 @@ def main():
         pass_list = passes_all(tle_list, observer["location"], observer["opts"])[:3]
 
         pass_list = list(map(lambda x: {
-            "start": unix2date(x["startUTC"]),
-            "max": unix2date(x["maxUTC"]),
-            "end": unix2date(x["endUTC"]),
+            "start": timestamp(x["startUTC"]),
+            "max": timestamp(x["maxUTC"]),
+            "end": timestamp(x["endUTC"]),
             "num": x["satnum"],
             "name": x["satname"],
             "freq": x["freq"],
@@ -44,9 +56,11 @@ def main():
         # pp(pass_list)
 
         nxt = pass_list[0]
-        print(f"At {nxt['start']} {nxt['name']} will pass over until {nxt['end']}, frequency: {nxt['freq']} MHz")
-        print(f"Schedule a task to tune into {nxt['freq']} MHz at {nxt['start']}")
-        print(f"At {nxt['end']}, wrap up image, send it to discord and get the next pass and schedule it")
+        print(f"At {readable(nxt['start'])} {nxt['name']} will pass over until {readable(nxt['end'])}, frequency: {nxt['freq']} MHz")
+        print(f"Schedule a task to tune into {nxt['freq']} MHz at {readable(nxt['start'])}")
+        print(f"At {readable(nxt['end'])}, wrap up image, send it to discord and get the next pass and schedule it")
+        pause.until(nxt["start"])
+        task(nxt["name"], nxt["freq"], nxt["end"])
 
 
 if __name__ == '__main__':
