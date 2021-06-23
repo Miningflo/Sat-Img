6c30ac27977fd5a2ab1f876922e1cd476cf6219e
Miningflo <debosscher.flore@gmail.com>
2021-06-23 18:53:31 +0200

    Updating discord_hook.py

--- discord_hook.py
+++ discord_hook.py
@@ -1,19 +1,41 @@
+import base64
+import json
 import config
 import requests
 
 file = open("testimg.txt", 'r')
 content = file.read()
 file.close()
-print(content)
+
+
+def create_payload(payload):
+    return json.dumps({
+        "username": payload["username"],
+        "embeds": [
+            {
+                "title": "Pass at " + payload["time"],
+                "image": {
+                    "url": "attachment://" + payload["image"]
+                },
+            }
+        ]
+    })
 
 
 hook = config.load()["discord-webhook"]
-headers = {"Content-Type": "multipart/form-data"}
 payload = {
-    "content": "test",
-    "file": content
+    "username": "NOAA 18",
+    "time": "17:20",
+    "image": "img.jpg"
 }
 
-x = requests.post(hook, headers=headers, data=payload)
+files = [
+    ('file1', (payload["image"], base64.decodebytes(open('testimg.txt', 'rb').read()))),
+]
+data = [
+    ('payload_json', create_payload(payload))
+]
 
-print(x.text)
+response = requests.post(hook, files=files, data=data)
+
+print(response.text)
