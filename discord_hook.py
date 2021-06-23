6cf988b3935b3fe61f4ff59a73de9f622691486a
Miningflo <debosscher.flore@gmail.com>
2021-06-23 19:05:44 +0200

    Updating discord_hook.py

--- discord_hook.py
+++ discord_hook.py
@@ -3,19 +3,25 @@ import json
 import config
 import requests
 
-file = open("testimg.txt", 'r')
-content = file.read()
-file.close()
+
+def send_message(message_data):
+    files = [
+        ('file1', ("generic_name." + message_data["image_ext"], message_data["image_data"])),
+    ]
+    data = [
+        ('payload_json', create_payload(message_data))
+    ]
+    requests.post(message_data["hook"], files=files, data=data)
 
 
-def create_payload(payload):
+def create_payload(message_data):
     return json.dumps({
-        "username": payload["username"],
+        "username": message_data["satname"],
         "embeds": [
             {
-                "title": "Pass at " + payload["time"],
+                "title": "Pass at " + message_data["time"],
                 "image": {
-                    "url": "attachment://" + payload["image"]
+                    "url": "attachment://generic_name." + message_data["image_ext"]
                 },
             }
         ]
@@ -23,19 +29,13 @@ def create_payload(payload):
 
 
 hook = config.load()["discord-webhook"]
-payload = {
-    "username": "NOAA 18",
+image = base64.decodebytes(open('testimg.txt', 'rb').read())
+message = {
+    "hook": hook,
+    "satname": "NOAA 18",
     "time": "17:20",
-    "image": "img.jpg"
+    "image_ext": "jpg",
+    "image_data": image
 }
 
-files = [
-    ('file1', (payload["image"], base64.decodebytes(open('testimg.txt', 'rb').read()))),
-]
-data = [
-    ('payload_json', create_payload(payload))
-]
-
-response = requests.post(hook, files=files, data=data)
-
-print(response.text)
+send_message(message)
