6624042c3e9c94aa7e250b287a64edcb0de3a4e5
Miningflo <debosscher.flore@gmail.com>
2021-06-27 10:56:16 +0200

    Updated resampler.py to work with functions, simplifying workflow

--- wx2img/resampler.py
+++ wx2img/resampler.py
@@ -1,8 +1,13 @@
 import numpy as np
 from scipy.io import wavfile
 from scipy.signal import resample as rs
-import math
 import matplotlib.pyplot as plt
+
+
+def read_file(filename):
+    samplerate, data = wavfile.read(filename)
+    data = pad_data(data, samplerate)
+    return samplerate, data
 
 
 def pad_data(data, modulo):
@@ -15,23 +20,28 @@ def resample(data, current_rate, target_rate):
     return pad_data(rs(data, target_sample_count), target_rate)
 
 
+def filter(data):
+    return (data - np.min(data)) / (np.max(data) - np.min(data))
+
+
+def draw(data):
+    # og size: 1040
+    # good size: 1791
+    width = 2388
+    rows = pad_data(data, width)
+    rows = np.reshape(rows, (-1, width))
+
+    plt.matshow(rows, cmap=plt.cm.gray)
+    plt.show()
+
+
 input_file = "./test.wav"
 
-samplerate, data = wavfile.read(input_file)
+samplerate, data = read_file(input_file)
 
-data = pad_data(data, samplerate)
 resampled_9600 = resample(data, samplerate, 9600)
 instantaneous_4800 = [(data[i] ** 2 + data[i + 1] ** 2) ** (1 / 2) for i in range(0, len(resampled_9600), 2)]
 result_4160 = resample(instantaneous_4800, 4800, 4160)
-print(len(result_4160))
-filtered = (result_4160 - np.min(result_4160)) / (np.max(result_4160) - np.min(result_4160)) * 255
-print(filtered)
+filtered = filter(result_4160) * 255
 
-# og size: 1040
-# good size: 1791
-width = 2388
-rows = pad_data(filtered, width)
-rows = np.reshape(rows, (-1, width))
-
-plt.matshow(rows, cmap=plt.cm.gray)
-plt.show()
+draw(filtered)
