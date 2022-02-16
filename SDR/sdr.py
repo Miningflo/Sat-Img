17836de540278a760a31b895a209c3b7d198e595
Flore De Bosscher <debosscher.flore@gmail.com>
2022-02-16 17:37:55 +0100

    Update sdr.py

--- SDR/sdr.py
+++ SDR/sdr.py
@@ -9,7 +9,7 @@ def listen(frequency, duration):
     sdr.sample_rate = sample_rate  # Hz
     sdr.center_freq = frequency * 1e6  # Hz
     sdr.bandwidth = 60e3  # Hz
-    sdr.gain = 'auto'
+    sdr.gain = 'auto' #19.7dB works for my device
 
     samples = sdr.read_samples(sample_rate * duration)
     sdr.close()
